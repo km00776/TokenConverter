3cbc15da4d2ed788e9e7972435d761f190d74a5a
karthi.murugathas@waracle.com <karthi.murugathas@waracle.com>
2025-08-08 15:08:40 +0100

    update index

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // index.js
 
 // Import the themes you built under build/js
-import { theme as lightTheme } from './build/js/light/theme-light.js';
-import { theme as darkTheme } from './build/js/dark/theme-dark.js';
+import { theme as lightTheme } from './build/js/light/theme-light.ts';
+import { theme as darkTheme } from './build/js/dark/theme-dark.ts';
 
 // Re-export them so consumers can do:
 //   import { lightTheme, darkTheme } from '@km00776/design-tokens';
